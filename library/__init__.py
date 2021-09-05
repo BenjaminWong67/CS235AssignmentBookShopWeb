e7241cc33cba5d2e5ad220f44993fd5e16ac42b0
Timothy <timtam8083@gmail.com>
2021-09-05 12:26:56 +1200

    **

--- library/__init__.py
+++ library/__init__.py
@@ -2,30 +2,44 @@
 
 from flask import Flask, render_template
 
-# TODO: Access to the books should be implemented via the repository pattern and using blueprints, so this can not stay here!
-from library.domain.model import Book
-
-# TODO: Access to the books should be implemented via the repository pattern and using blueprints, so this can not stay here!
-def create_some_book():
-    some_book = Book(1, "Harry Potter and the Chamber of Secrets")
-    some_book.description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean \
-                             and hideous that all Harry wanted was to get back to the Hogwarts School for \
-                             Witchcraft and Wizardry. But just as he’s packing his bags, Harry receives a \
-                             warning from a strange impish creature who says that if Harry returns to Hogwarts, \
-                             disaster will strike."
-    some_book.release_year = 1999
-    return some_book
+import library.adapters.repository as repo
+from library.adapters.memoryrepository import MemoryRepository, populate
 
 
-def create_app():
+#        # TODO: Access to the books should be implemented via the repository pattern and using blueprints, so this can not stay here!
+#        def create_some_book():
+#            some_book = Book(1, "Harry Potter and the Chamber of Secrets")
+#            some_book.description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean \
+#                                     and hideous that all Harry wanted was to get back to the Hogwarts School for \
+#                                     Witchcraft and Wizardry. But just as he’s packing his bags, Harry receives a \
+#                                     warning from a strange impish creature who says that if Harry returns to Hogwarts, \
+#                                     disaster will strike."
+#            some_book.release_year = 1999
+#            return some_book
+
+def create_app(test_config=None):
     app = Flask(__name__)
 
+    # sets up normal configuration
     app.config.from_object('config.Config')
 
+    # setting up testing configuration
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
+
+    # creates Memory repo instance
+    repo.repo_instance = MemoryRepository()
+    # fills the repo instance with the provided data_path
+    populate(data_path, repo.repo_instance)
+
+
     @app.route('/')
-    def home():
-        some_book = create_some_book()
-        # Use Jinja to customize a predefined html page rendering the layout for showing a single book.
-        return render_template('simple_book.html', book=some_book)
+    def page_of_books():
+        pass
+
+    @app.route('/book')
+    def book_page():
+        pass
 
     return app
