affea8c2272cf731859832873648425d9ee46299
Timothy <timtam8083@gmail.com>
2021-09-09 10:23:46 +1200

    blueprint for search branch

--- library/books/books.py
+++ library/books/books.py
@@ -1,3 +1,9 @@
 """
-books blueprint will go here
+books blueprint will go here:
+
+develop app-route for search here
+
+-things to think about
+-should a user be able to search with multiple fields e.g.
+   -publisher and author at the same time
 """
