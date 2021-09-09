65fc4d3e879d69902ca7500edf51a6cf120cad95
Timothy <timtam8083@gmail.com>
2021-09-09 12:08:23 +1200

    setup blueprint configuration

--- library/authentication/authentication.py
+++ library/authentication/authentication.py
@@ -1,3 +1,6 @@
-"""
-authentication blueprint will go here
-"""
+from flask import Blueprint, render_template, redirect, url_for, session, request
+
+
+# Configure Blueprint.
+authentication_blueprint = Blueprint(
+    'authentication_bp', __name__, url_prefix='/authentication')
--- library/books/books.py
+++ library/books/books.py
@@ -7,3 +7,9 @@ develop app-route for search here
 -should a user be able to search with multiple fields e.g.
    -publisher and author at the same time
 """
+from flask import Blueprint, render_template, redirect, url_for, session, request
+
+
+# configure blueprint
+books_blueprint = Blueprint(
+    "books_bp", __name__, url_prefix="/books")
