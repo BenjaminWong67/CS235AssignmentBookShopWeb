370efe1d95720503669e82e84738dce734c25f06
Timothy <timtam8083@gmail.com>
2021-09-09 21:31:18 +1200

    book search one attribute at a time

--- library/books/books.py
+++ library/books/books.py
@@ -2,10 +2,6 @@
 books blueprint will go here:
 
 develop app-route for search here
-
--things to think about
--should a user be able to search with multiple fields e.g.
-   -publisher and author at the same time
 """
 from flask import Blueprint, render_template, redirect, url_for, session, request
 
