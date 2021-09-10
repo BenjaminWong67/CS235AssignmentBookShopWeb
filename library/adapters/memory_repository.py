41198a3bbb70a9ed2fe382ec058bbd166675ba17
Timothy <timtam8083@gmail.com>
2021-09-10 20:55:24 +1200

    edited mem repo search doc comment

--- library/adapters/memory_repository.py
+++ library/adapters/memory_repository.py
@@ -21,24 +21,35 @@ class MemoryRepository(AbstractRepository):
         }
 
     def add_book(self, book: Book):
+        """
+        adds book to the repo list and index dictionary
+        also indexes the book into the search dictionary
+
+        if the book is already in the list... nothing happens
+        a value error is raised when a non book item is passed
+        """
         if isinstance(book, Book):
             if book.book_id not in self.__books_index.keys():
                 self.__books.append(book)
                 self.__books_index[book.book_id] = book
 
+                # index title
                 if book.title not in self.__search_dictionary["title"].keys():
                     self.__search_dictionary["title"][book.title] = list()
                 self.__search_dictionary["title"][book.title].append(book.book_id)
 
+                # index author
                 for author in book.authors:
                     if str(author.full_name) not in self.__search_dictionary["author"].keys():
                         self.__search_dictionary["author"][str(author.full_name)] = list()
                     self.__search_dictionary["author"][str(author.full_name)].append(book.book_id)
 
+                # index publisher
                 if str(book.publisher.name) not in self.__search_dictionary["publisher"].keys():
                     self.__search_dictionary["publisher"][str(book.publisher.name)] = list()
                 self.__search_dictionary["publisher"][str(book.publisher.name)].append(book.book_id)
 
+                # index release year
                 if str(book.release_year) not in self.__search_dictionary["release_year"].keys():
                     self.__search_dictionary["release_year"][str(book.release_year)] = list()
                 self.__search_dictionary["release_year"][str(book.release_year)].append(book.book_id)
