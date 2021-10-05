e1311f9a0bf6f336a81580fdf5c018624936a851
Timothy <timtam8083@gmail.com>
2021-10-05 13:57:25 +1300

    moved review under user within code to fix user not defined error in parameters

--- library/domain/model.py
+++ library/domain/model.py
@@ -237,58 +237,6 @@ class Book:
         return hash(self.book_id)
 
 
-class Review:
-
-    def __init__(self, book: Book, review_text: str, rating: int, user):
-        if isinstance(book, Book):
-            self.__book = book
-        else:
-            self.__book = None
-
-        if isinstance(review_text, str):
-            self.__review_text = review_text.strip()
-        else:
-            self.__review_text = "N/A"
-
-        if isinstance(rating, int) and rating >= 1 and rating <= 5:
-            self.__rating = rating
-        else:
-            raise ValueError
-
-        self.__timestamp = datetime.now()
-        self.__user = user
-
-    @property
-    def book(self) -> Book:
-        return self.__book
-
-    @property
-    def review_text(self) -> str:
-        return self.__review_text
-
-    @property
-    def rating(self) -> int:
-        return self.__rating
-
-    @property
-    def timestamp(self) -> datetime:
-        return self.__timestamp
-
-    @property
-    def user(self):
-        return self.__user
-
-    def __eq__(self, other):
-        if not isinstance(other, self.__class__):
-            return False
-
-        return other.book == self.book and other.review_text == self.review_text \
-               and other.rating == self.rating and other.timestamp == self.timestamp
-
-    def __repr__(self):
-        return f'<Review of book {self.book}, rating = {self.rating}, timestamp = {self.timestamp}>'
-
-
 class User:
 
     def __init__(self, user_name: str, password: str):
@@ -374,6 +322,58 @@ class User:
 
     def __hash__(self):
         return hash(self.user_name)
+
+
+class Review:
+
+    def __init__(self, book: Book, review_text: str, rating: int, user: User):
+        if isinstance(book, Book):
+            self.__book = book
+        else:
+            self.__book = None
+
+        if isinstance(review_text, str):
+            self.__review_text = review_text.strip()
+        else:
+            self.__review_text = "N/A"
+
+        if isinstance(rating, int) and rating >= 1 and rating <= 5:
+            self.__rating = rating
+        else:
+            raise ValueError
+
+        self.__timestamp = datetime.now()
+        self.__user = user
+
+    @property
+    def book(self) -> Book:
+        return self.__book
+
+    @property
+    def review_text(self) -> str:
+        return self.__review_text
+
+    @property
+    def rating(self) -> int:
+        return self.__rating
+
+    @property
+    def timestamp(self) -> datetime:
+        return self.__timestamp
+
+    @property
+    def user(self):
+        return self.__user
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return other.book == self.book and other.review_text == self.review_text \
+               and other.rating == self.rating and other.timestamp == self.timestamp
+
+    def __repr__(self):
+        return f'<Review of book {self.book}, rating = {self.rating}, timestamp = {self.timestamp}>'
 
 
 class BooksInventory:
